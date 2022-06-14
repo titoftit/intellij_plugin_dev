869f1da48db46e1c4f557d70515a6e728b1324f5
GitHub Action <action@github.com>
2022-06-14 13:08:40 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/titoftit/intellijplugindev/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.titoftit.intellijplugindev.services
+
+import com.github.titoftit.intellijplugindev.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/titoftit/intellijplugindev/services/MyProjectService.kt
+++ src/main/kotlin/com/github/titoftit/intellijplugindev/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.titoftit.intellijplugindev.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.titoftit.intellijplugindev.MyBundle
 
 class MyProjectService(project: Project) {
 
